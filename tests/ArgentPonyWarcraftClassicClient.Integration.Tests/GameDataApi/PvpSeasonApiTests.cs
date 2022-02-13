9ff35ff6e1fd15525c4c246a0bfac30a79612ad1
Andreas Teizel <andreas.teizel@posteo.de>
2022-02-13 16:28:57 +0100

    Changed season in Tests to 2, since season 1 is currently not working (404 Not Found)

--- tests/ArgentPonyWarcraftClassicClient.Integration.Tests/GameDataApi/PvpSeasonApiTests.cs
+++ tests/ArgentPonyWarcraftClassicClient.Integration.Tests/GameDataApi/PvpSeasonApiTests.cs
@@ -18,10 +18,10 @@ public class PvpSeasonApiTests
     {
         IPvpSeasonApi warcraftClient = ClientFactory.BuildClient();
 
-        RequestResult<PvpSeason> result = await warcraftClient.GetPvpSeasonAsync(1, "dynamic-classic-us");
+        RequestResult<PvpSeason> result = await warcraftClient.GetPvpSeasonAsync(2, "dynamic-classic-us");
 
         await result.Should().BeSuccessfulRequest()
-            .BeEquivalentToBlizzardResponseAsync("https://us.api.blizzard.com/data/wow/pvp-season/1?namespace=dynamic-classic-us&locale=en_US");
+            .BeEquivalentToBlizzardResponseAsync("https://us.api.blizzard.com/data/wow/pvp-season/2?namespace=dynamic-classic-us&locale=en_US");
     }
 
     [ResilientFact]
@@ -29,10 +29,10 @@ public class PvpSeasonApiTests
     {
         IPvpSeasonApi warcraftClient = ClientFactory.BuildClient();
 
-        RequestResult<PvpLeaderboardsIndex> result = await warcraftClient.GetPvpLeaderboardsIndexAsync(1, 1, "dynamic-classic-us");
+        RequestResult<PvpLeaderboardsIndex> result = await warcraftClient.GetPvpLeaderboardsIndexAsync(1, 2, "dynamic-classic-us");
 
         await result.Should().BeSuccessfulRequest()
-            .BeEquivalentToBlizzardResponseAsync("https://us.api.blizzard.com/data/wow/pvp-region/1/pvp-season/1/pvp-leaderboard/index?namespace=dynamic-classic-us&locale=en_US");
+            .BeEquivalentToBlizzardResponseAsync("https://us.api.blizzard.com/data/wow/pvp-region/1/pvp-season/2/pvp-leaderboard/index?namespace=dynamic-classic-us&locale=en_US");
     }
 
     [ResilientFact(Skip = "Needs work after porting from retail.")]
@@ -51,9 +51,9 @@ public class PvpSeasonApiTests
     {
         IPvpSeasonApi warcraftClient = ClientFactory.BuildClient();
 
-        RequestResult<PvpRewardsIndex> result = await warcraftClient.GetPvpRewardsIndexAsync(1, 1, "dynamic-classic-us");
+        RequestResult<PvpRewardsIndex> result = await warcraftClient.GetPvpRewardsIndexAsync(1, 2, "dynamic-classic-us");
 
         await result.Should().BeSuccessfulRequest()
-            .BeEquivalentToBlizzardResponseAsync("https://us.api.blizzard.com/data/wow/pvp-region/1/pvp-season/1/pvp-reward/index?namespace=dynamic-classic-us&locale=en_US");
+            .BeEquivalentToBlizzardResponseAsync("https://us.api.blizzard.com/data/wow/pvp-region/1/pvp-season/2/pvp-reward/index?namespace=dynamic-classic-us&locale=en_US");
     }
 }
