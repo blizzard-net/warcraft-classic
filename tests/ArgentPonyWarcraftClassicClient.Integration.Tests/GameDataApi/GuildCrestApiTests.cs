19b733e5b749e69ebc2070d132f549194b2df1c3
Andreas Teizel <andreas.teizel@posteo.de>
2022-04-03 12:50:28 +0200

    Guild Crest API reviewed

--- tests/ArgentPonyWarcraftClassicClient.Integration.Tests/GameDataApi/GuildCrestApiTests.cs
+++ tests/ArgentPonyWarcraftClassicClient.Integration.Tests/GameDataApi/GuildCrestApiTests.cs
@@ -2,36 +2,79 @@
 
 public class GuildCrestApiTests
 {
+    string namespaceBCC = "static-classic-us";
+    Locale localeBCC = Locale.en_US;
+    string hostBCC = "us.api.blizzard.com";
+
+    string namespaceEra = "static-classic1x-eu";
+    Region regionEra = Region.Europe;
+    Locale localeEra = Locale.en_US;
+    string hostEra = "eu.api.blizzard.com";
+
     [ResilientFact]
-    public async Task GetGuildCrestComponentsIndexAsync_Gets_GuildCrestComponentsIndex()
+    public async Task GetGuildCrestComponentsIndexAsync_Gets_GuildCrestComponentsIndex_ForBCC()
     {
         IGuildCrestApi warcraftClient = ClientFactory.BuildClient();
 
-        RequestResult<GuildCrestComponentsIndex> result = await warcraftClient.GetGuildCrestComponentsIndexAsync("static-classic-us");
+        RequestResult<GuildCrestComponentsIndex> result = await warcraftClient.GetGuildCrestComponentsIndexAsync(namespaceBCC);
 
         await result.Should().BeSuccessfulRequest()
-            .BeEquivalentToBlizzardResponseAsync("https://us.api.blizzard.com/data/wow/guild-crest/index?namespace=static-classic-us&locale=en_US");
+            .BeEquivalentToBlizzardResponseAsync($"https://{hostBCC}/data/wow/guild-crest/index?namespace={namespaceBCC}&locale={localeBCC}");
     }
 
     [ResilientFact]
-    public async Task GetGuildCrestBorderMediaAsync_Gets_GuildCrestBorderMedia()
+    public async Task GetGuildCrestBorderMediaAsync_Gets_GuildCrestBorderMedia_ForBCC()
     {
         IGuildCrestApi warcraftClient = ClientFactory.BuildClient();
 
-        RequestResult<GuildCrestBorderMedia> result = await warcraftClient.GetGuildCrestBorderMediaAsync(0, "static-classic-us");
+        RequestResult<GuildCrestBorderMedia> result = await warcraftClient.GetGuildCrestBorderMediaAsync(0, namespaceBCC);
 
         await result.Should().BeSuccessfulRequest()
-            .BeEquivalentToBlizzardResponseAsync("https://us.api.blizzard.com/data/wow/media/guild-crest/border/0?namespace=static-classic-us&locale=en_US");
+            .BeEquivalentToBlizzardResponseAsync($"https://{hostBCC}/data/wow/media/guild-crest/border/0?namespace={namespaceBCC}&locale={localeBCC}");
     }
 
     [ResilientFact]
-    public async Task GetGuildCrestEmblemMediaAsync_Gets_GuildCrestEmblemMedia()
+    public async Task GetGuildCrestEmblemMediaAsync_Gets_GuildCrestEmblemMedia_ForBCC()
     {
         IGuildCrestApi warcraftClient = ClientFactory.BuildClient();
 
-        RequestResult<GuildCrestEmblemMedia> result = await warcraftClient.GetGuildCrestEmblemMediaAsync(0, "static-classic-us");
+        RequestResult<GuildCrestEmblemMedia> result = await warcraftClient.GetGuildCrestEmblemMediaAsync(0, namespaceBCC);
 
         await result.Should().BeSuccessfulRequest()
-            .BeEquivalentToBlizzardResponseAsync("https://us.api.blizzard.com/data/wow/media/guild-crest/emblem/0?namespace=static-classic-us&locale=en_US");
+            .BeEquivalentToBlizzardResponseAsync($"https://{hostBCC}/data/wow/media/guild-crest/emblem/0?namespace={namespaceBCC}&locale={localeBCC}");
     }
+
+    [ResilientFact]
+    public async Task GetGuildCrestComponentsIndexAsync_Gets_GuildCrestComponentsIndex_ForEra()
+    {
+        IGuildCrestApi warcraftClient = ClientFactory.BuildClient();
+
+        RequestResult<GuildCrestComponentsIndex> result = await warcraftClient.GetGuildCrestComponentsIndexAsync(namespaceEra, regionEra, localeEra);
+
+        await result.Should().BeSuccessfulRequest()
+            .BeEquivalentToBlizzardResponseAsync($"https://{hostEra}/data/wow/guild-crest/index?namespace={namespaceEra}&locale={localeEra}");
+    }
+
+    [ResilientFact]
+    public async Task GetGuildCrestBorderMediaAsync_Gets_GuildCrestBorderMedia_ForEra()
+    {
+        IGuildCrestApi warcraftClient = ClientFactory.BuildClient();
+
+        RequestResult<GuildCrestBorderMedia> result = await warcraftClient.GetGuildCrestBorderMediaAsync(0, namespaceEra, regionEra, localeEra);
+
+        await result.Should().BeSuccessfulRequest()
+            .BeEquivalentToBlizzardResponseAsync($"https://{hostEra}/data/wow/media/guild-crest/border/0?namespace={namespaceEra}&locale={localeEra}");
+    }
+
+    [ResilientFact]
+    public async Task GetGuildCrestEmblemMediaAsync_Gets_GuildCrestEmblemMedia_ForEra()
+    {
+        IGuildCrestApi warcraftClient = ClientFactory.BuildClient();
+
+        RequestResult<GuildCrestEmblemMedia> result = await warcraftClient.GetGuildCrestEmblemMediaAsync(0, namespaceEra, regionEra, localeEra);
+
+        await result.Should().BeSuccessfulRequest()
+            .BeEquivalentToBlizzardResponseAsync($"https://{hostEra}/data/wow/media/guild-crest/emblem/0?namespace={namespaceEra}&locale={localeEra}");
+    }
+
 }
--- tests/ArgentPonyWarcraftClassicClient.Tests/GameDataApi/GuildCrestApiTests.cs
+++ tests/ArgentPonyWarcraftClassicClient.Tests/GameDataApi/GuildCrestApiTests.cs
@@ -2,36 +2,74 @@
 
 public class GuildCrestApiTests
 {
+    string namespaceBCC = "static-classic-us";
+    string namespaceEra = "static-classic1x-eu";
+
+
     [Fact]
-    public async Task GetGuildCrestComponentsIndexAsync_Gets_GuildCrestComponentsIndex()
+    public async Task GetGuildCrestComponentsIndexAsync_Gets_GuildCrestComponentsIndex_ForBCC()
     {
         IGuildCrestApi warcraftClient = ClientFactory.BuildMockClient(
-            requestUri: "https://us.api.blizzard.com/data/wow/guild-crest/index?namespace=static-classic-us&locale=en_US",
+            requestUri: $"https://us.api.blizzard.com/data/wow/guild-crest/index?namespace={namespaceBCC}&locale=en_US",
             responseContent: Resources.GuildCrestComponentsIndexResponse_ForBCC);
 
-        RequestResult<GuildCrestComponentsIndex> result = await warcraftClient.GetGuildCrestComponentsIndexAsync("static-classic-us");
+        RequestResult<GuildCrestComponentsIndex> result = await warcraftClient.GetGuildCrestComponentsIndexAsync(namespaceBCC);
         Assert.NotNull(result.Value);
     }
 
     [Fact]
-    public async Task GetGuildCrestBorderMediaAsync_Gets_GuildCrestBorderMedia()
+    public async Task GetGuildCrestBorderMediaAsync_Gets_GuildCrestBorderMedia_ForBCC()
     {
         IGuildCrestApi warcraftClient = ClientFactory.BuildMockClient(
-            requestUri: "https://us.api.blizzard.com/data/wow/media/guild-crest/border/0?namespace=static-classic-us&locale=en_US",
+            requestUri: $"https://us.api.blizzard.com/data/wow/media/guild-crest/border/0?namespace={namespaceBCC}&locale=en_US",
             responseContent: Resources.GuildCrestBorderMediaResponse_ForBCC);
 
-        RequestResult<GuildCrestBorderMedia> result = await warcraftClient.GetGuildCrestBorderMediaAsync(0, "static-classic-us");
+        RequestResult<GuildCrestBorderMedia> result = await warcraftClient.GetGuildCrestBorderMediaAsync(0, namespaceBCC);
         Assert.NotNull(result.Value);
     }
 
     [Fact]
-    public async Task GetGuildCrestEmblemMediaAsync_Gets_GuildCrestEmblemMedia()
+    public async Task GetGuildCrestEmblemMediaAsync_Gets_GuildCrestEmblemMedia_ForBCC()
     {
         IGuildCrestApi warcraftClient = ClientFactory.BuildMockClient(
-            requestUri: "https://us.api.blizzard.com/data/wow/media/guild-crest/emblem/0?namespace=static-classic-us&locale=en_US",
+            requestUri: $"https://us.api.blizzard.com/data/wow/media/guild-crest/emblem/0?namespace={namespaceBCC}&locale=en_US",
             responseContent: Resources.GuildCrestEmblemMediaResponse_ForBCC);
 
-        RequestResult<GuildCrestEmblemMedia> result = await warcraftClient.GetGuildCrestEmblemMediaAsync(0, "static-classic-us");
+        RequestResult<GuildCrestEmblemMedia> result = await warcraftClient.GetGuildCrestEmblemMediaAsync(0, namespaceBCC);
         Assert.NotNull(result.Value);
     }
+
+    [Fact]
+    public async Task GetGuildCrestComponentsIndexAsync_Gets_GuildCrestComponentsIndex_ForEra()
+    {
+        IGuildCrestApi warcraftClient = ClientFactory.BuildMockClient(
+            requestUri: $"https://us.api.blizzard.com/data/wow/guild-crest/index?namespace={namespaceEra}&locale=en_US",
+            responseContent: Resources.GuildCrestComponentsIndexResponse_ForEra);
+
+        RequestResult<GuildCrestComponentsIndex> result = await warcraftClient.GetGuildCrestComponentsIndexAsync(namespaceEra);
+        Assert.NotNull(result.Value);
+    }
+
+    [Fact]
+    public async Task GetGuildCrestBorderMediaAsync_Gets_GuildCrestBorderMedia_ForEra()
+    {
+        IGuildCrestApi warcraftClient = ClientFactory.BuildMockClient(
+            requestUri: $"https://us.api.blizzard.com/data/wow/media/guild-crest/border/0?namespace={namespaceEra}&locale=en_US",
+            responseContent: Resources.GuildCrestBorderMediaResponse_ForEra);
+
+        RequestResult<GuildCrestBorderMedia> result = await warcraftClient.GetGuildCrestBorderMediaAsync(0, namespaceEra);
+        Assert.NotNull(result.Value);
+    }
+
+    [Fact]
+    public async Task GetGuildCrestEmblemMediaAsync_Gets_GuildCrestEmblemMedia_ForEra()
+    {
+        IGuildCrestApi warcraftClient = ClientFactory.BuildMockClient(
+            requestUri: $"https://us.api.blizzard.com/data/wow/media/guild-crest/emblem/0?namespace={namespaceEra}&locale=en_US",
+            responseContent: Resources.GuildCrestEmblemMediaResponse_ForEra);
+
+        RequestResult<GuildCrestEmblemMedia> result = await warcraftClient.GetGuildCrestEmblemMediaAsync(0, namespaceEra);
+        Assert.NotNull(result.Value);
+    }
+
 }
