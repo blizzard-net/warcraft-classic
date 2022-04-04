78df6a2aa88a8d3babcfc4155a498894aa6c2856
Andreas Teizel <andreas.teizel@posteo.de>
2022-04-04 18:22:31 +0200

    Reviewed Playable Race API

--- tests/ArgentPonyWarcraftClassicClient.Integration.Tests/GameDataApi/PlayableRaceApiTests.cs
+++ tests/ArgentPonyWarcraftClassicClient.Integration.Tests/GameDataApi/PlayableRaceApiTests.cs
@@ -2,25 +2,58 @@
 
 public class PlayableRaceApiTests
 {
+    string namespaceBCC = "static-classic-us";
+    Locale localeBCC = Locale.en_US;
+    string hostBCC = "us.api.blizzard.com";
+
+    string namespaceEra = "static-classic1x-eu";
+    Region regionEra = Region.Europe;
+    Locale localeEra = Locale.en_US;
+    string hostEra = "eu.api.blizzard.com";
+
     [ResilientFact]
-    public async Task GetPlayableRacesIndexAsync_Gets_PlayableRacesIndex()
+    public async Task GetPlayableRacesIndexAsync_Gets_PlayableRacesIndex_ForBCC()
     {
         IPlayableRaceApi warcraftClient = ClientFactory.BuildClient();
 
-        RequestResult<PlayableRacesIndex> result = await warcraftClient.GetPlayableRacesIndexAsync("static-classic-us");
+        RequestResult<PlayableRacesIndex> result = await warcraftClient.GetPlayableRacesIndexAsync(namespaceBCC);
 
         await result.Should().BeSuccessfulRequest()
-            .BeEquivalentToBlizzardResponseAsync("https://us.api.blizzard.com/data/wow/playable-race/index?namespace=static-classic-us&locale=en_US");
+            .BeEquivalentToBlizzardResponseAsync($"https://{hostBCC}/data/wow/playable-race/index?namespace={namespaceBCC}&locale={localeBCC}");
     }
 
     [ResilientFact]
-    public async Task GetPlayableRaceAsync_Gets_PlayableRace()
+    public async Task GetPlayableRaceAsync_Gets_PlayableRace_ForBCC()
     {
         IPlayableRaceApi warcraftClient = ClientFactory.BuildClient();
 
-        RequestResult<PlayableRace> result = await warcraftClient.GetPlayableRaceAsync(2, "static-classic-us");
+        RequestResult<PlayableRace> result = await warcraftClient.GetPlayableRaceAsync(2, namespaceBCC);
 
         await result.Should().BeSuccessfulRequest()
-            .BeEquivalentToBlizzardResponseAsync("https://us.api.blizzard.com/data/wow/playable-race/2?namespace=static-classic-us&locale=en_US");
+            .BeEquivalentToBlizzardResponseAsync($"https://{hostBCC}/data/wow/playable-race/2?namespace={namespaceBCC}&locale={localeBCC}");
     }
+
+
+    [ResilientFact]
+    public async Task GetPlayableRacesIndexAsync_Gets_PlayableRacesIndex_ForEra()
+    {
+        IPlayableRaceApi warcraftClient = ClientFactory.BuildClient();
+
+        RequestResult<PlayableRacesIndex> result = await warcraftClient.GetPlayableRacesIndexAsync(namespaceEra, regionEra, localeEra);
+
+        await result.Should().BeSuccessfulRequest()
+            .BeEquivalentToBlizzardResponseAsync($"https://{hostEra}/data/wow/playable-race/index?namespace={namespaceEra}&locale={localeEra}");
+    }
+
+    [ResilientFact]
+    public async Task GetPlayableRaceAsync_Gets_PlayableRace_ForEra()
+    {
+        IPlayableRaceApi warcraftClient = ClientFactory.BuildClient();
+
+        RequestResult<PlayableRace> result = await warcraftClient.GetPlayableRaceAsync(2, namespaceEra, regionEra, localeEra);
+
+        await result.Should().BeSuccessfulRequest()
+            .BeEquivalentToBlizzardResponseAsync($"https://{hostEra}/data/wow/playable-race/2?namespace={namespaceEra}&locale={localeEra}");
+    }
+
 }
